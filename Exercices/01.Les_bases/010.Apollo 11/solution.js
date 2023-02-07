604c992a51a125e744ffc3d3d19e274b44a306eb
Alexluu13 <alexluu13@gmail.com>
2023-02-07 17:56:52 +0100

    new exercice Js

---
+++ Exercices/01.Les_bases/010.Apollo 11/solution.js
@@ -0,0 +1,41 @@
+// Le 16 juillet 1969 à Cape Kennedy en Floride a 
+// eu lieu le décollage d'Apollo 11. Pour la première 
+// fois, des Hommes allaient se poser sur la Lune ...
+
+// Affichez dans la console les commentaires de [Jack King]
+// (https://en.wikipedia.org/wiki/Jack_King_(NASA)) à 
+// l'aide d'une boucle. La boucle devra décrémenter un 
+// compteur de 12 à 0.
+
+// Nom du fichier à créer : **apollo11.js**
+
+// Résultat attendu dans le terminal :
+
+// 12
+// 11
+// 10
+// 9
+// Ignition sequence start
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// 0
+// All engines running
+// Liftoff! 🚀
+
+///////////////////////////////////////////////
+const limit = 0;
+
+for (let i = 12; i >= limit; i--) {
+    if (i === 8) {
+    console.log("Ignition sequence start");
+    } else if (i !== 7) {
+    console.log(i);
+    }
+}
+
+console.log("All engines running\nLiftoff! 🚀");
+///////////////////////////////////////////////
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JavaScript_Exercices
+
+https://incongruous-ash-ac2.notion.site/Exercices-JavaScript-73330812488846ed828c2981ab5250b6
